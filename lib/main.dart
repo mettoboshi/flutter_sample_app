1608905daa9eaf05a0991c0e4c879f9f335408d2
Yasuo Fujita <mettoboshi.1108@gmail.com>
2022-05-08 16:04:12 +0900

    横に等間隔に配置してみる

--- lib/main.dart
+++ lib/main.dart
@@ -48,6 +48,26 @@ class _MyHomePageState extends State<MyHomePage> {
           TextButton(
             onPressed: () => {print("test")},
             child: const Text("更新"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: const [
+              Icon(
+                Icons.favorite,
+                color: Colors.pink,
+                size: 24.0,
+              ),
+              Icon(
+                Icons.audiotrack,
+                color: Colors.green,
+                size: 30.0,
+              ),
+              Icon(
+                Icons.beach_access,
+                color: Colors.blue,
+                size: 36.0,
+              )
+            ],
           )
         ]));
   }
